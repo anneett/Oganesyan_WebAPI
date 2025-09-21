8a17c099b0bdf0fb71dda7fc4f6870331a63333e
anneett <ann_oganesyan59@mail.ru>
2025-09-21 17:52:06 +0300

    Добавьте файлы проекта.

---
+++ Models/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace Oganesyan_WebAPI.Models
+{
+    public class Solution
+    {
+        //public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TaskId { get; set; }
+        public string UserAnswer { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace Oganesyan_WebAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string Difficulty { get; set; }
+
+    }
+}
